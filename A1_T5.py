db45f391ade5e2e21faa8cd8e11d0fe2854b8c3a
Arttu Eklund <arttu.eklund@student.lab.fi>
2025-09-15 10:36:57 +0300

    Hienosäätöä

--- A1_T5.py
+++ A1_T5.py
@@ -1,8 +1,8 @@
 print("Calculate the area of a wall.")
 Feed = input("Enter the width in meters: ")
-Width = round(float(Feed))
+Width = float(Feed)
 Feed = input("Enter the height in meters: ")
-Height = round(float(Feed))
+Height = float(Feed)
 print("Width is", Width, "m and height is", Height, "m.")
 Area = Width * Height
 print("The wall will be", Area, "square meters.")
